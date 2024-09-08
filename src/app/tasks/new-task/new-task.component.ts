3802fe04a312897449230ee5ccaa3135c97bd352
rezwan <rezwanshahriorrahin@gmail.com>
2024-09-08 19:04:53 +0600

    change: add a new Input as 'userId' in 'new-task' component
    - bind with 'task' component template

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type NewTaskData } from '../task/task.model';
 import { TasksService } from '../tasks.service';
@@ -11,6 +11,7 @@ import { TasksService } from '../tasks.service';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  @Input({ required: true }) userId!: string;
   @Output() close = new EventEmitter<void>();
   @Output() add = new EventEmitter<NewTaskData>();
   enteredTitle = '';
