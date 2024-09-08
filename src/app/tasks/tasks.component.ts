a6f689f105bdaaaa352f0f93da7f8c08aeacedda
rezwan <rezwanshahriorrahin@gmail.com>
2024-09-08 19:15:51 +0600

    change: remove 'onAddTask' method from 'tasks' component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
-import { type NewTaskData } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
@@ -31,18 +30,6 @@ export class TasksComponent {
   }
 
   onCloseAddTask() {
-    this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    });
-
     this.isAddingTask = false;
   }
 }
